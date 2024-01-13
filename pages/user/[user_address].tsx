21089ed071362c8910a9b080ff030b50cd4bc47a
xlassix <awhy14539@gmail.com>
2024-01-13 16:36:23 +0100

    Profile Page Added

---
+++ pages/user/[user_address].tsx
@@ -0,0 +1,16 @@
+import { Text } from "@chakra-ui/react";
+import { SWRConfig } from "swr";
+
+const UserPage = () => {
+    return <SWRConfig value={{}}>
+        <Text>
+            UserPage
+        </Text>
+    </SWRConfig>
+}
+
+
+
+
+
+export default UserPage;
